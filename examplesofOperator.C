7cc08d6edbbab17ff41d1b8dd11d42382366ec94
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-04 21:32:15 +0530

    Add files via upload

---
+++ examplesofOperator.C
@@ -0,0 +1,18 @@
+// If it's Monday or it's raining: example of how we use operators.
+
+#include <stdio.h>
+
+
+
+       int main() {
+ 
+     
+       int isMonday =1;
+ 
+       int isRaining =0;
+  
+       printf("%d", isMonday||isRaining);
+ 
+  return 0;
+
+}
